d454cb01173c117e0e8e3d331174ae277f8d78b8
D-GUBBS <swiswikubheka@gmail.com>
2024-04-05 20:07:34 +0200

    added api

--- style.css/java.js
+++ style.css/java.js
@@ -1,10 +1,22 @@
-function searchCity(city) {}
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "f395ab75a23faa33c4066t05fao79b4d";
+  let upiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 
